f240a29730d6ef3f10896b3f5075782cc62031cc
Florin <fiathf@yahoo.com>
2021-04-12 14:14:14 +0200

    Merge branch 'main' of github.com:FbW43-2/React_Final_Project

--- moviesearch_app/src/index.js
+++ moviesearch_app/src/index.js
@@ -1,23 +1,23 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import "bootstrap/dist/css/bootstrap.min.css";
-import './style.css';
 import { Provider } from 'react-redux'
 import { createStore, applyMiddleware } from 'redux'
 import thunk from 'redux-thunk';
 
-import reducers from "./reducers";
+import reducers from "./reducer";
 
 
 
 const App = () => {
     return (
         <div>
-            
+            Hello
         </div>
     );
 };
 
-ReactDOM.render(<Provider store={createStore(reducers, applyMiddleware(thunk))}>
+ReactDOM.render(
+<Provider store={createStore(reducers, applyMiddleware(thunk))}>
     <App />
 </Provider>, document.getElementById('root'))
