e0d823959dc24062fbda900b6aa67db2df8080b0
Florin <fiathf@yahoo.com>
2021-04-13 21:01:22 +0200

    small changes

--- moviesearch_app/src/index.js
+++ moviesearch_app/src/index.js
@@ -2,16 +2,19 @@ import React from "react";
 import ReactDOM from "react-dom";
 import "bootstrap/dist/css/bootstrap.min.css";
 import { Provider } from 'react-redux';
-import { Router, Switch, Route } from "react-router";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import { createStore, applyMiddleware } from 'redux'
 import thunk from 'redux-thunk';
-import './style.css';
+import './css/style.css';
 
 
 import reducers from "./reducer";
 
-import Menu from './components/menu';
-import HomePage from "./pages/homepage";
+import Navbar from "./components/navbar.js";
+import Home from "./pages/home";
+import Search from "./pages/search";
+import Movies from "./pages/movies";
+import Series from "./pages/series"; 
 
 
 
@@ -47,32 +50,5 @@ ReactDOM.render(
     <App />
   </Provider>,
   document.getElementById("root")
-);
-
-    return (
-<<<<<<< HEAD
-      <Router>
-        <Menu />
-        <Switch>
-          <Route exact path="/">
-            <HomePage/>
-          </Route>
-          
-        </Switch>
-      </Router>
-=======
-        <div>
-
-   
-            <Menu/>
-
-        </div>
->>>>>>> 59e448184af38794a9b377c29721f53e1f2f4d74
-    );
-};
-
-ReactDOM.render(
-<Provider store={createStore(reducers, applyMiddleware(thunk))}>
-    <App />
-</Provider>, document.getElementById('root'))
+)
 
