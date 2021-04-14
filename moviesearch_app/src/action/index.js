02996570617e287fbde376c48d36829b1c9dbe9d
Florin <fiathf@yahoo.com>
2021-04-14 10:32:55 +0200

    defined action and reducer

---
+++ moviesearch_app/src/action/index.js
@@ -0,0 +1,25 @@
+import {getResults} from  '../api'
+
+export const getMovieData = (movie) => {
+
+
+return dispatch => {
+
+ dispatch ({
+        type: 'START',
+        payload: []
+    })
+ getResults(movie).then(data => {
+        dispatch({
+          type: "SUCCESS",
+          payload: data
+        })
+ }).catch(error => {
+     dispatch({
+        type: 'FAILED',
+        payload: error
+     })
+ })
+}
+
+}
--- moviesearch_app/src/reducer/index.js
+++ moviesearch_app/src/reducer/index.js
@@ -1,11 +1,32 @@
 import { combineReducers } from "redux";
 
-const searchResult = [];
 
-const searchReducer = (result = searchReducer, action) => {
-  return result;
+const searchResult = {
+  data: [],
+  status: ''
+}
+
+const searchReducer = (results = searchResult, action) => {
+  
+  console.log(action);
+
+  switch (action.type) {
+
+    case 'START':
+      searchResult = { data: [] , status :'START'};
+      return {...searchResult};
+    case 'SUCCESS':
+      searchResult = { data: action.payload, status :'SUCCESS'};
+      return {...searchResult};
+    case 'FAILED':
+      searchResult = { data: [], status: 'FAILED'};
+      return {...searchResult};
+    default:
+      return results;
+  }
+
 };
 
 export default combineReducers({
-  result: searchReducer,
+  results: searchReducer
 });
