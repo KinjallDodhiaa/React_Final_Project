b8416cf8304337a7074885a8f8f10a297e2bdbf9
Kevin Knop <knopkevkev@gmail.com>
2021-04-16 21:41:20 +0200

    some style changes

--- moviesearch_app/src/components/moviecategories.js
+++ moviesearch_app/src/components/moviecategories.js
@@ -1,7 +1,7 @@
 import React from "react";
 import { Button, Container, Row } from "react-bootstrap";
 import { getMovieData } from "../action";
-import {connect} from 'react-redux'
+import { connect } from 'react-redux'
 
 const MovieCategories = ({ getMovieData }) => {
   const searchMovieCategoryOnClick = (e) => {
@@ -10,45 +10,107 @@ const MovieCategories = ({ getMovieData }) => {
 
   return (
     <div>
-      <Container>
+      <Container className="mt-5">
         <Row>
-          <h1>Movies</h1>
-          <Button name="action" onClick={searchMovieCategoryOnClick}>
-            Action
-          </Button>
-          <Button name="thriller" onClick={searchMovieCategoryOnClick}>
-            Thriller
-          </Button>
-          <Button name="horror" onClick={searchMovieCategoryOnClick}>
-            Horror
-          </Button>
-          <Button name="drama" onClick={searchMovieCategoryOnClick}>
-            Drama
-          </Button>
-          <Button name="comedy" onClick={searchMovieCategoryOnClick}>
-            Comedy
-          </Button>
-          <Button name="documentary" onClick={searchMovieCategoryOnClick}>
-            Documentary
-          </Button>
-          <Button name="animation" onClick={searchMovieCategoryOnClick}>
-            Animation
-          </Button>
-          <Button name="anime" onClick={searchMovieCategoryOnClick}>
-            Anime
-          </Button>
-          <Button name="science fiction" onClick={searchMovieCategoryOnClick}>
-            Science fiction
-          </Button>
-          <Button name="cartoon" onClick={searchMovieCategoryOnClick}>
-            Cartoon
-          </Button>
-          <Button name="crime" onClick={searchMovieCategoryOnClick}>
-            Crime
-          </Button>
-          <Button name="love" onClick={searchMovieCategoryOnClick}>
-            Romance
-          </Button>
+          <div className="seriesBtnGroup">
+            <h1 className="series">Movies</h1>
+            <button
+              type="button"
+              class="btn btn-dark"
+              name="action"
+              onClick={searchMovieCategoryOnClick}
+            >
+              Comedy
+            </button>
+            <button
+              type="button"
+              class="btn btn-dark"
+              name="thriller"
+              onClick={searchMovieCategoryOnClick}
+            >
+              Thriller
+            </button>
+            <button
+              type="button"
+              class="btn btn-dark"
+              name="horror"
+              onClick={searchMovieCategoryOnClick}
+            >
+              Horror
+            </button>
+            <button
+              type="button"
+              class="btn btn-dark"
+              name="drama"
+              onClick={searchMovieCategoryOnClick}
+            >
+              Drama
+            </button>
+            <button
+              type="button"
+              class="btn btn-dark"
+              name="comedy"
+              onClick={searchMovieCategoryOnClick}
+            >
+              Animation
+            </button>
+            <button
+              type="button"
+              class="btn btn-dark"
+              name="documentary"
+              onClick={searchMovieCategoryOnClick}
+            >
+              Documentary
+            </button>
+            <button
+              type="button"
+              class="btn btn-dark"
+              name="animation"
+              onClick={searchMovieCategoryOnClick}
+            >
+              Animation
+            </button>
+            <button
+              type="button"
+              class="btn btn-dark"
+              name="anime"
+              onClick={searchMovieCategoryOnClick}
+            >
+              Anime
+            </button>
+            <button
+              type="button"
+              class="btn btn-dark"
+              name="science fiction"
+              onClick={searchMovieCategoryOnClick}
+            >
+              SF
+            </button>
+            <button
+              type="button"
+              class="btn btn-dark"
+              name="cartoon"
+              onClick={searchMovieCategoryOnClick}
+            >
+              Cartoon
+            </button>
+            <button
+              type="button"
+              class="btn btn-dark"
+              name="crime"
+              onClick={searchMovieCategoryOnClick}
+            >
+              Crime
+            </button>
+            <button
+              type="button"
+              class="btn btn-dark"
+              name="love"
+              onClick={searchMovieCategoryOnClick}
+            >
+              Love
+            </button>
+          </div>
         </Row>
       </Container>
     </div>
